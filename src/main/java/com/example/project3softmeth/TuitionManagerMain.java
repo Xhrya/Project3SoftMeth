3d4cee366cc5f5503bac52a06de2dce1e357f9b7
Shreya Pandey <42945783+Xhrya@users.noreply.github.com>
2023-03-13 17:55:45 -0400

    everything except scholarship

---
+++ src/main/java/com/example/project3softmeth/TuitionManagerMain.java
@@ -0,0 +1,23 @@
+package com.example.project3softmeth;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TuitionManagermain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(TuitionManagermain.class.getResource("tuition_manager_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
+        stage.setTitle("Project 3 - Tuition Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

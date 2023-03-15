13cb50712daea78a3268c4c708be84c9c613f0ac
Medhasri Veldurthi <medhasri.vel@gmail.com>
2023-03-14 20:29:23 -0400

    buttons + text reading

--- src/main/java/com/example/project3softmeth/TuitionManagerController.java
+++ src/main/java/com/example/project3softmeth/TuitionManagerController.java
@@ -15,6 +15,12 @@ public class TuitionManagerController {
     @FXML
     ToggleGroup majorButtons;
     @FXML
+    TextField firstNameRoster;
+    @FXML
+    TextField lastNameRoster;
+    @FXML
+    DatePicker birthDate;
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
@@ -23,6 +29,9 @@ public class TuitionManagerController {
     protected void onAddButtonClick(Event e) {
         System.out.println(residentButtons.getSelectedToggle().toString());
         System.out.println(majorButtons.getSelectedToggle().toString());
+        System.out.println(firstNameRoster.getText());
+        System.out.println(lastNameRoster.getText());
+        System.out.println(birthDate.getValue());
     }
 }
 

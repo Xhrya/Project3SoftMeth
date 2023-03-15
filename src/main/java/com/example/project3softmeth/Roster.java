ce4b770d21280c2ca18bfcbd8ab78795008b4166
Medhasri Veldurthi <medhasri.vel@gmail.com>
2023-03-15 01:20:42 -0400

    Push roster add,drop,changemajor except load from file

---
+++ src/main/java/com/example/project3softmeth/Roster.java
@@ -0,0 +1,279 @@
+/**
+ * @author Shreya Pandey
+ * @author Medhasri Veldurthi
+ */
+package com.example.project3softmeth;
+
+public class Roster {
+    private Student[] roster;
+    private int size;
+
+    public Roster() {
+        this.roster = roster;
+        this.size = size;
+    }
+
+    /**
+     * Making a roster of students
+     *
+     * @param r Array of students
+     * @param s size of the student array
+     */
+    public Roster(Student[] r, int s) {
+        this.roster = r;
+        this.size = s;
+    }
+
+    /**
+     * Finds the index of a student we are looking for
+     *
+     * @param student student we are searching for
+     * @return index of the student that is being searched
+     */
+    private int find(Student student) {
+        int NOT_FOUND = -1;
+        for (int i = 0; i < size; i++) {
+            if (roster[i].equals(student)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    } //search the given student in roster
+
+
+
+    /**
+     * Grows the size of the student array by 4
+     */
+    private void grow() {
+        Student[] newArray = new Student[size + 4];
+
+        if (roster != null) {
+            for (int i = 0; i <= size; i++) {
+                newArray[i] = roster[i];
+            }
+        }
+
+
+        roster = newArray;
+    } //increase the array capacity by 4
+
+    /**
+     * Gives the number of students in the array
+     *
+     * @return Number of students in the array
+     */
+    public int getSize() {
+        return size;
+    }
+
+
+    /**
+     * Adds a student to the roster
+     *
+     * @param student Student to be added
+     * @return True of student has been added
+     */
+    public boolean add(Student student) {
+        if (roster == null) {
+            grow();
+        }
+        if (roster.length - 1 == size) {
+            grow();
+        }
+
+        roster[size] = student;
+        size++;
+
+        if(student == null)
+        {
+            return false;
+        }
+        return true;
+
+
+    } //add student to end of array
+
+    /**
+     * @param student Student we are removing
+     * @return true if student successfully removed
+     */
+    public boolean remove(Student student) {
+        for (int i = 0; i <= size - 1; i++) {
+            if (roster[i].equals(student)) {
+                roster[i] = null;
+                for (int j = i; j < size - 1; j++) {
+                    roster[j] = roster[j + 1];
+                }
+                roster[size - 1] = null;
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }//maintain the order after remove
+
+    /**
+     * Checks if student is in the roster
+     *
+     * @param student Student we are searching for
+     * @return boolean which is true if student is in the array
+     */
+    public boolean contains(Student student) {
+        for (int i = 0; i <= size - 1; i++) {
+            if (find(student) != -1) {
+                return true;
+            }
+        }
+        return false;
+    } //if the student is in roster
+
+    /**
+     * Finds the student in the roster given a profile
+     *
+     * @param p profile of the student
+     * @return profile of the student given the profile
+     */
+    public Student ProfileToStudent(Profile p) {
+        for (int i = 0; i < size; i++) {
+            if (roster[i].getProfile().toString().equals(p.toString())) {
+                return roster[i];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Prints the student roster
+     */
+    public void print() {
+        //selection sort on print...not sure if this works
+        for (int i = 0; i < size; i++) {
+            int min = i;
+            for (int j = 1 + i; j < size; j++) {
+                if ((roster[j].compareTo(roster[min])) == -1) {
+                    min = j;
+                }
+            }
+            Student temp = roster[min];
+            roster[min] = roster[i];
+            roster[i] = temp;
+        }
+        for (int k = 0; k < size; k++) {
+            System.out.println(roster[k]);
+        }
+
+    } //print roster sorted by profiles
+
+
+    /**
+     * Prints students first by their School, then by their Major
+     */
+    public void printBySchoolMajor() {
+        for (int i = 0; i < size; i++) {
+            int min = i;
+            for (int j = 1 + i; j < size; j++) {
+                String schoolMajorJ = roster[j].getSchool() + roster[j].getMajor().toString();
+                String schoolMajorMin = roster[min].getSchool() + roster[min].getMajor().toString();
+                if (schoolMajorJ.compareTo(schoolMajorMin) < 0) {
+                    min = j;
+                }
+            }
+            Student temp = roster[min];
+            roster[min] = roster[i];
+            roster[i] = temp;
+        }
+        for (int k = 0; k < size; ++k) {
+            System.out.println(roster[k]);
+        }
+
+
+    } //print roster sorted by school major
+
+    /**
+     * Prints students for a specific school
+     *
+     * @param school Given a specific school to print for
+     */
+    public void printBySchool(String school)  //ex L SAS
+    {
+        for (int i = 0; i < size; i++) {
+            if (roster[i].getSchool().equals(school)) {
+                System.out.println((roster[i]));
+            }
+        }
+
+    }
+
+
+    /**
+     * Prints students by their standing(Freshman, sophomore, junior, senior)
+     */
+    public void printByStanding() {
+        for (int i = 0; i < size; i++) {
+            int min = i;
+            for (int j = 1 + i; j < size; j++) {
+                if ((roster[j].compareTo(roster[min])) == -1) {
+                    min = j;
+                }
+            }
+            Student temp = roster[min];
+            roster[min] = roster[i];
+            roster[i] = temp;
+        }
+
+        for (int i = 0; i < size; i++) {
+            int min = i;
+            for (int j = 1 + i; j < size; j++) {
+
+                if (roster[j].getYear().compareTo((roster[min].getYear())) < 0) {
+
+                    min = j;
+                }
+            }
+            Student temp = roster[min];
+            roster[min] = roster[i];
+            roster[i] = temp;
+        }
+
+        //first take this class ordered tog by all years
+        //then create another loop and order tog by names
+        for (int k = 0; k < size; k++) {
+
+            String output = "";
+            output = roster[k] + " (" + roster[k].getYear() + ")";
+            System.out.println(output);
+        }
+
+    } //print roster sorted by standing
+
+    /**
+     * Changes the major of the Student
+     *
+     * @param p        Given a profile of a student who's major to change
+     * @param newMajor Assigning a new major to the student
+     * @return true if major has successfully been changed
+     */
+    public boolean ChangeMajor(Profile p, Major newMajor) {
+        //find in roster based on profile
+        for (int i = 0; i < size; i++) {
+            if (roster[i].getProfile().toString().equals(p.toString())) {
+                roster[i].setMajor(newMajor);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Resident findResident(Profile p){
+        int NOT_FOUND = -1;
+        for (int i = 0; i < size; i++) {
+            if (roster[i].getProfile().equals(p)) {
+                return (Resident) roster[i];
+            }
+        }
+        return null;
+    }
+
+}
+
--- src/main/java/com/example/project3softmeth/TuitionManagerController.java
+++ src/main/java/com/example/project3softmeth/TuitionManagerController.java
@@ -9,18 +9,22 @@ import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
 import java.io.File;
-import java.util.Arrays;
 
 public class TuitionManagerController {
-
     //everything for RosteR:
-    @FXML
-    private Label welcomeText;
+//    @FXML
+//    private Label welcomeText;
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+
+    Roster newRoster = new Roster();
+    Enrollment enrollmentList = new Enrollment();
     @FXML
     ToggleGroup residentButtons;
     @FXML
     ToggleGroup regionButtons;
-
     @FXML
     ToggleGroup majorButtons;
     @FXML
@@ -28,57 +32,275 @@ public class TuitionManagerController {
     @FXML
     TextField lastNameRoster;
     @FXML
+    TextField creditsCompletedRoster;
+    @FXML
     DatePicker birthDate;
-
+    @FXML
+    RadioButton isAbroad;
     @FXML
     DatePicker birthDateEnroll;
 
 
-
     //everything for Enroll/Drop
-
-
-
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
     @FXML
     protected void onAddButtonClick(Event e) {
-        System.out.println(residentButtons.getSelectedToggle().toString());
+       //converting the toggle buttons of resident and major into strings
         String residency = residentButtons.getSelectedToggle().toString();
         residency = residency.substring(residency.indexOf("'") +1, residency.length() -1);
 
-        System.out.println(residency);
+        String major = majorButtons.getSelectedToggle().toString();
+        major = major.substring(major.indexOf("'") +1, major.length() -1);
+
+
+        //delete this later!
+        System.out.println(residentButtons.getSelectedToggle().toString());
+        System.out.println("STATUS:"+residency);
         System.out.println(majorButtons.getSelectedToggle().toString());
+        System.out.println("MAJOR:" + major);
         System.out.println(firstNameRoster.getText());
         System.out.println(lastNameRoster.getText());
+        System.out.println(creditsCompletedRoster.getText());
         System.out.println(birthDate.getValue());
+//        String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+//        System.out.println(dob);
+        //delete this later^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
-        if(residency.equals("Resident"))
-        {
-            System.out.println("resident 1");
-        }
-        else if(residentButtons.getSelectedToggle().toString().equals("NonResident"))
-        {
-            System.out.println("non 1");
-        }
-       else if(residentButtons.getSelectedToggle().toString().equals("Tristate"))
-        {
-            System.out.println("tri 1");
-        }
-        else if(residentButtons.getSelectedToggle().toString().equals("International"))
-        {
-            System.out.println("inter 1");
-        }
+        if(residency.equals("Resident")) {
+            //NEED AN EXCEPTION TO MAKE SURE ALL VALUES ARE ENTERED AND NONE OF THE FIELDS ARE MISSING
+            System.out.println("entered resident statement");
+            String firstName = firstNameRoster.getText();
+            String lastName = lastNameRoster.getText();
+            String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+            int credits = 0;
+            try{
+                credits = Integer.parseInt((creditsCompletedRoster.getText()));
+            } catch( NumberFormatException creditE) {
+                System.out.println("Credits completed invalid: not an integer!");
+                return;
+            }
+            //current date + checking if valid
+            Date d = new Date(dob);
+            boolean isValid = d.isValid();
 
+            //making a student profile and age is valid
+            Profile thisStudent = new Profile(lastName, firstName, d);
+            int age = thisStudent.getAge();
+
+            //checking for valid DOB + age, adding an ENUM if this exists
+            if (age > 16) {
+                if (isValid) {
+                    Major studentMajorEnum = Major.valueOf(major);
+                    //making sure credits are positive
+                    if (credits >= 0) {
+                        //checking if student is in the roster, then adding student meeting all requirements
+                        Resident newResident = new Resident(thisStudent, studentMajorEnum, credits);
+                        if (!newRoster.contains(newResident)) {
+                            newRoster.add(newResident);
+                            System.out.println(thisStudent.toString() + " added to the roster.");
+                        } else {
+                            System.out.println(thisStudent.toString() + " is already in the roster.");
+                        }
+                    } else {
+                        System.out.println("Credits completed invalid: cannot be negative!");
+                    }
+                } else {
+                    System.out.println("DOB invalid: " + dob.toString() + " not a valid calendar date!");
+                }
+            } else {
+                System.out.println("DOB invalid: " + dob.toString() + " is younger than 16 years old.");
+            }
+        }else if(residency.equals("NonResident")) {
+            System.out.println("Nonresident entered");
+            //NEED AN EXCEPTION TO MAKE SURE ALL VALUES ARE ENTERED AND NONE OF THE FIELDS ARE MISSING
+            String firstName = firstNameRoster.getText();
+            String lastName = lastNameRoster.getText();
+            String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+            int credits = 0;
+            try{
+                credits = Integer.parseInt((creditsCompletedRoster.getText()));
+            } catch( NumberFormatException creditE) {
+                System.out.println("Credits completed invalid: not an integer!");
+                return;
+            }
+            //current date + checking if valid
+            Date d = new Date(dob);
+            boolean isValid = d.isValid();
+
+            //making a student profile and age is valid
+            Profile thisStudent = new Profile(lastName, firstName, d);
+            int age = thisStudent.getAge();
+
+            //checking for valid DOB + age, adding an ENUM if this exists
+            if (age > 16) {
+                if (isValid) {
+                        Major studentMajorEnum = Major.valueOf(major);
+                        //making sure credits are positive
+                        if (credits >= 0) {
+                            //checking if student is in the roster, then adding student meeting all requirements
+                            Resident newNonResident = new Resident(thisStudent, studentMajorEnum, credits);
+                            if (!newRoster.contains(newNonResident)) {
+                                newRoster.add(newNonResident);
+                                System.out.println(thisStudent.toString() + " added to the roster.");
+                            } else {
+                                System.out.println(thisStudent.toString() + " is already in the roster.");
+                            }
+                        } else {
+                            System.out.println("Credits completed invalid: cannot be negative!");
+                        }
+                } else {
+                    System.out.println("DOB invalid: " + dob.toString() + " not a valid calendar date!");
+                }
+            } else {
+                System.out.println("DOB invalid: " + dob.toString() + " is younger than 16 years old.");
+            }
+
+        } else if(residency.equals("Tristate")) {
+            System.out.println("triSTATE entered");
+            String firstName = firstNameRoster.getText();
+            String lastName = lastNameRoster.getText();
+            String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+            String state = (regionButtons.getSelectedToggle().toString());
+            state = state.substring(state.indexOf("'") +1, state.length() -1);
+            System.out.println("STATE:" + state);
+
+            int credits = 0;
+            try{
+                credits = Integer.parseInt((creditsCompletedRoster.getText()));
+            } catch( NumberFormatException creditE) {
+                System.out.println("Credits completed invalid: not an integer!");
+                return;
+            }
+            //current date + checking if valid
+            Date d = new Date(dob);
+            boolean isValid = d.isValid();
+
+            //making a student profile and age is valid
+            Profile thisStudent = new Profile(lastName, firstName, d);
+            int age = thisStudent.getAge();
+
+            //checking for valid DOB + age, adding an ENUM if this exists
+            if (age > 16) {
+                if (isValid) {
+                        Major studentMajorEnum = Major.valueOf(major);
+                        //making sure credits are positive
+                        if (credits >= 0) {
+                            //checking if student is in the roster, then adding student meeting all requirements
+                            TriState newTriState = new TriState(thisStudent, studentMajorEnum, credits, state);
+                            if (!(newRoster.contains(newTriState))) {
+                                newRoster.add(newTriState);
+                                //can create an instance of the bigger object and assign a smaller object to it
+
+                                System.out.println(thisStudent.toString() + " added to the roster.");
+                            } else {
+                                System.out.println(thisStudent.toString() + " is already in the roster.");
+                            }
+                        } else {
+                            System.out.println("Credits completed invalid: cannot be negative!");
+                        }
+                } else {
+                    System.out.println("DOB invalid: " + dob.toString() + " not a valid calendar date!");
+                }
+            } else {
+                System.out.println("DOB invalid: " + dob.toString() + " is younger than 16 years old.");
+            }
+        } else if(residency.equals("International")) {
+            System.out.println("international entered");
+            String firstName = firstNameRoster.getText();
+            String lastName = lastNameRoster.getText();
+            String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+            boolean abroad = isAbroad.isSelected();
+
+            int credits = 0;
+            try{
+                credits = Integer.parseInt((creditsCompletedRoster.getText()));
+            } catch( NumberFormatException creditE) {
+                System.out.println("Credits completed invalid: not an integer!");
+                return;
+            }
+            //current date + checking if valid
+            Date d = new Date(dob);
+            boolean isValid = d.isValid();
+
+            //making a student profile and age is valid
+            Profile thisStudent = new Profile(lastName, firstName, d);
+            int age = thisStudent.getAge();
+
+            if (age > 16) {
+                if (isValid) {
+                        Major studentMajorEnum = Major.valueOf(major);
+                        //making sure credits are positive
+                        if (credits >= 0) {
+                            //checking if student is in the roster, then adding student meeting all requirements
+                            International i = new International(thisStudent, studentMajorEnum, credits, abroad);
+                            if (!newRoster.contains(i)) {
+                                newRoster.add(i);
+                                System.out.println(thisStudent.toString() + " added to the roster.");
+                            } else {
+                                System.out.println(thisStudent.toString() + " is already in the roster.");
+                            }
+                        } else {
+                            System.out.println("Credits completed invalid: cannot be negative!");
+                        }
+                } else {
+                    System.out.println("DOB invalid: " + dob.toString() + " not a valid calendar date!");
+                }
+            } else {
+                System.out.println("DOB invalid: " + dob.toString() + " is younger than 16 years old.");
+            }
+
+
+        }
 
     }
 
-//    @FXML
+
+    @FXML
+    protected void onDropButtonClick(Event e) {
+        String firstName = firstNameRoster.getText();
+        String lastName = lastNameRoster.getText();
+        String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+
+        //making the Date, Profile, and Student
+        Date RMDate = new Date(dob);
+        Profile RMProfile = new Profile(lastName, firstName, RMDate);
+        Student RMStudent = newRoster.ProfileToStudent(RMProfile);
+        boolean isRemoved = newRoster.remove(RMStudent);
+        if (isRemoved) {
+            System.out.println(RMProfile.toString() + " removed from the roster.");
+        } else {
+            System.out.println(RMProfile.toString() + " is not in the roster.");
+        }
+    }
+
+    @FXML
+    protected void onChangeMajorClick(Event e) {
+        String major = majorButtons.getSelectedToggle().toString();
+        major = major.substring(major.indexOf("'") +1, major.length() -1);
+
+        String firstName = firstNameRoster.getText();
+        String lastName = lastNameRoster.getText();
+        String dob = birthDate.getValue().toString().substring(5 , 7) + "/" + birthDate.getValue().toString().substring(8 , 10) + "/" + birthDate.getValue().toString().substring(0 , 4);
+
+        //making the Date & Profile
+        Date CDate = new Date(dob);
+        Profile CProfile = new Profile(lastName, firstName, CDate);
+        Major MajorEnum = Major.valueOf(major);
+
+        //Changing the major of the student
+        boolean isChanged = newRoster.ChangeMajor(CProfile, MajorEnum);
+        if (isChanged) {
+            System.out.println(CProfile.toString() + " major changed to " + major.toString());
+        } else {
+            System.out.println("Student not found");
+        }
+    }
+
+    @FXML
+    protected void onLoadFromFile(Event e) {//attach to scene builder
+
+    }
+    //    @FXML
 //    VBox vbMenu;
 
 //    @FXML
@@ -100,12 +322,6 @@ public class TuitionManagerController {
 //
 //        }
 //    }
-
-    @FXML
-    protected void OnDropButtonClick(Event e)
-    {//attach to scene builder
-
-    }
 
 
 }
